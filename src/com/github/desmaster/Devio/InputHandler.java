4abcf1d6f12d57975a306b690ce571fc39dde8b2
Timon de Groot <tdegroot96>
2012-11-03 22:18:34 +0100

    Cleared the ticking and polling up

--- src/com/github/desmaster/Devio/InputHandler.java
+++ src/com/github/desmaster/Devio/InputHandler.java
@@ -4,50 +4,36 @@ import org.lwjgl.input.Keyboard;
 
 public class InputHandler {
 	
-	private Devio game;
+	public float x = 0;
+	public float y = 0;
+	
+	//private Devio game;
+	public boolean up = false;
+	public boolean left = false;
+	public boolean down = false;
+	public boolean right = false;
+	public boolean exit = false;
+	Thread thread;
 	
 	public InputHandler(Devio game) {
-		this.game = game;
+		//this.game = game;
 	}
 
 	public void tick() {
+		up = false;
+		left = false;
+		down = false;
+		right = false;
+		exit = false;
 		pollInput();
 	}
-
+	
 	public void pollInput() {
-		if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) game.stop();
-		while (Keyboard.next()) {
-			if (Keyboard.getEventKeyState()) {
-				
-				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
-
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
-
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_W) {
-
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
-
-				}
-				
-			} else {
-				
-				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
-
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
-
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_W) {
-
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
-
-				}
-			}
-		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) exit = true;
+		if (Keyboard.isKeyDown(Keyboard.KEY_A)) left = true;
+		if (Keyboard.isKeyDown(Keyboard.KEY_D)) right = true;
+		if (Keyboard.isKeyDown(Keyboard.KEY_W)) up = true;
+		if (Keyboard.isKeyDown(Keyboard.KEY_S)) down = true;
 	}
 
 }
