bc3a8271df1e8dbd93c3dcf4cf1684068b6f6d87
Timon de Groot <tdegroot96>
2012-11-03 22:19:55 +0100

    Implemented Player class and added render methods

--- src/com/github/desmaster/Devio/gfx/Screen.java
+++ src/com/github/desmaster/Devio/gfx/Screen.java
@@ -1,5 +1,7 @@
 package com.github.desmaster.Devio.gfx;
 
+import com.github.desmaster.Devio.InputHandler;
+import com.github.desmaster.Devio.entity.Player;
 import com.github.desmaster.Devio.realm.Level;
 import com.github.desmaster.Devio.texture.iTexture;
 
@@ -7,17 +9,25 @@ public class Screen {
 
 	private iTexture texture;
 	private Level level;
+	private Player player;
+	private InputHandler input;
 
-	public Screen() {
-		setTexture(new iTexture());
+	public Screen(InputHandler input) {
+		texture = new iTexture();
 		level = new Level();
+		player = new Player(iTexture.PLAYER_NINJA, 9, 6.7f, 100, input);
+		this.setInput(input);
+		input.x = player.x;
+		input.y = player.y;
 	}
 
 	public void render() {
 		level.render();
+		player.render();
 	}
 
 	public void tick() {
+		//player.tick();
 		
 	}
 
@@ -27,5 +37,13 @@ public class Screen {
 
 	public void setTexture(iTexture texture) {
 		this.texture = texture;
+	}
+
+	public InputHandler getInput() {
+		return input;
+	}
+
+	public void setInput(InputHandler input) {
+		this.input = input;
 	}
 }
