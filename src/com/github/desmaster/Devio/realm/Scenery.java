607d71ad7948ebdcd6fe8174d60e4302701f4854
Timon de Groot <tdegroot96>
2012-11-04 15:30:07 +0100

    Not done yet

--- src/com/github/desmaster/Devio/realm/Scenery.java
+++ src/com/github/desmaster/Devio/realm/Scenery.java
@@ -1,17 +1,18 @@
 package com.github.desmaster.Devio.realm;
 
-import static com.github.desmaster.Devio.realm.Level.BLOCKS_HEIGHT;
-import static com.github.desmaster.Devio.realm.Level.BLOCKS_WIDTH;
+import static com.github.desmaster.Devio.realm.Level.WORLD_HEIGHT;
+import static com.github.desmaster.Devio.realm.Level.WORLD_WIDTH;
 import static com.github.desmaster.Devio.realm.Level.BLOCK_SIZE;
 
 import com.github.desmaster.Devio.texture.iTexture;
 
-public class Scenery extends BlockGrid {
+public class Scenery extends World {
 
 	public Scenery() {
-		for (int x = 0; x < BLOCKS_WIDTH; x++) {
-			for (int y = 0; y < BLOCKS_HEIGHT; y++) {
+		for (int x = 0; x < WORLD_WIDTH; x++) {
+			for (int y = 0; y < WORLD_HEIGHT; y++) {
 				int seed = random.nextInt(iTexture.objects.length - random.nextInt(14));
+				//seed = random(0, iTexture.objects.length - random(0, 14));
 				if (random.nextInt(12 - random.nextInt(8)) == 1) {
 					if (seed == iTexture.ID_FLOWER_RED)
 						blocks[x][y] = new Block(iTexture.objects[seed], x * BLOCK_SIZE, y * BLOCK_SIZE);
