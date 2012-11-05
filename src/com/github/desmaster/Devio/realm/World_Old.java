3c865e2eb3ed2c359497884662e85fa3b3f9ad12
Timon de Groot <tdegroot96>
2012-11-05 20:27:07 +0100

    Renamed

---
+++ src/com/github/desmaster/Devio/realm/World_Old.java
@@ -0,0 +1,70 @@
+package com.github.desmaster.Devio.realm;
+
+import static com.github.desmaster.Devio.realm.Realm.BLOCK_SIZE;
+import static com.github.desmaster.Devio.realm.Realm.MAP_HEIGHT;
+import static com.github.desmaster.Devio.realm.Realm.MAP_WIDTH;
+import static com.github.desmaster.Devio.realm.Realm.WORLD_HEIGHT;
+import static com.github.desmaster.Devio.realm.Realm.WORLD_WIDTH;
+import static com.github.desmaster.Devio.tex.iTexture.ID_GRASS;
+import static com.github.desmaster.Devio.tex.iTexture.ID_SAND;
+import static com.github.desmaster.Devio.tex.iTexture.textures;
+
+import java.util.Random;
+
+import org.lwjgl.opengl.Display;
+import org.newdawn.slick.opengl.Texture;
+
+public class World_Old {
+
+	Block[][] blocks = new Block[WORLD_WIDTH][WORLD_HEIGHT];
+	Random random = new Random();
+
+	public World_Old() {
+		for (int x = 0; x < WORLD_WIDTH; x++) {
+			for (int y = 0; y < WORLD_HEIGHT; y++) {
+				if (x >= 0 && x < 10 || x >= 11 && x < Display.getWidth() - 1) {
+					blocks[x][y] = new Block(textures[random.nextInt(3)], x * BLOCK_SIZE,
+							y * BLOCK_SIZE);
+				} else {
+					blocks[x][y] = new Block(textures[ID_SAND], x * BLOCK_SIZE,
+							y * BLOCK_SIZE);
+				}
+			}
+		}
+		
+	}
+
+	public Block[][] generateWorld(int width, int height) {
+		Block[][] blocks = new Block[width][height];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				blocks[x][y] = new Block(textures[ID_GRASS], x * BLOCK_SIZE, y
+						* BLOCK_SIZE);
+			}
+		}
+		return blocks;
+	}
+
+	public int random(int a, int b) {
+		int dif = Math.abs(a - b);
+		return Math.min(a, b) + (dif <= 0 ? 0 : new Random().nextInt(dif));
+
+	}
+
+	public void setAt(int x, int y, Texture b) {
+		blocks[x][y] = new Block(b, x * BLOCK_SIZE, y * BLOCK_SIZE);
+	}
+
+	public Block getAt(int x, int y) {
+		return blocks[x][y];
+	}
+
+	public void render() {
+		for (int x = 0; x < MAP_WIDTH; x++) {
+			for (int y = 0; y < MAP_HEIGHT; y++) {
+				blocks[x][y].render();
+			}
+		}
+	}
+
+}
