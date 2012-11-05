a9a7e0c8ded61a7c4eb18093935a7cb6bdaf7869
BrainCode <h4cker99@gmail.com>
2012-11-05 20:24:52 +0100

    fixed lagbug in Tile.java
    did i fix it?

--- src/com/github/desmaster/Devio/realm/world/Tile.java
+++ src/com/github/desmaster/Devio/realm/world/Tile.java
@@ -6,41 +6,30 @@ import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
 import com.github.desmaster.Devio.cons.Console;
+import com.github.desmaster.Devio.tex.iTexture;
 
 public enum Tile {
-	GRASS("Grass", false, true), // 0
-	SAND("Sand", false, true), // 1
-	STONE("Stone", false, true), // 2
-	COBBLE_STONE("Cobblestone", false, true), // 3
-	MOSSY_STONE("Mossy_Cobblestone", false, true), // 4
-	WATER("Water", false, true); // 5
-	//LAVA("Lava", false, true) // 6
+	GRASS("Grass", false, true, 0), 					// 0
+	SAND("Sand", false, true, 1), 						// 1
+	STONE("Stone", false, true, 2), 					// 2
+	COBBLE_STONE("Cobblestone", false, true, 3), 		// 3
+	MOSSY_STONE("Mossy_Cobblestone", false, true, 4), 	// 4
+	WATER("Water", false, true, 5); 					// 5
+	//LAVA("Lava", false, true, 6) 						// 6
 
 	private String name;
 	private boolean breakable;
 	private boolean solid;
-	private Texture texture;
+	private int TextureID;
 	
 	public int x;
 	public int y;
 
-	Tile(String name, boolean breakable, boolean solid) {
+	Tile(String name, boolean breakable, boolean solid,int TextureID) {
 		this.name = name;
 		this.breakable = breakable;
 		this.solid = solid;
-		texture = loadTexture(name, "png");
-	}
-
-	private Texture loadTexture(String name, String ext) {
-		name = name.toLowerCase();
-		try {
-			Console.log("Loading " + name + "." + ext);
-			Texture t1 = TextureLoader.getTexture(ext.toUpperCase(), getClass().getResourceAsStream("/img/" + name + "." + ext));
-			return t1;
-		} catch (IOException e) {
-			Console.log(e.getMessage());
-		}
-		return null;
+		this.TextureID = TextureID;
 	}
 
 	public int getID() {
@@ -60,11 +49,13 @@ public enum Tile {
 	}
 
 	public Texture getTexture() {
-		return texture;
+		return iTexture.objects[TextureID];
 	}
-	
-	public void setTexture(Texture texture) {
-		this.texture = texture;
+	public int getTextureID() {
+		return TextureID;
+	}
+	public void setTextureID(int TextureID) {
+		this.TextureID = TextureID;
 	}
 
 }
