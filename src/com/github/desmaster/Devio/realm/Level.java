e33779b7777368c8885e6aa36764f9dd99635545
Timon de Groot <tdegroot96>
2012-11-05 15:24:21 +0100

    Fixed performance bug

--- src/com/github/desmaster/Devio/realm/Level.java
+++ src/com/github/desmaster/Devio/realm/Level.java
@@ -8,35 +8,34 @@ import com.github.desmaster.Devio.entity.Player;
 public class Level {
 
 	public static final int BLOCK_SIZE = 32;
-	public static final int MAP_WIDTH = 0;
-	public static final int MAP_HEIGHT = 0;
-	public static final int WORLD_WIDTH = Display.getWidth() / BLOCK_SIZE;
-	public static final int WORLD_HEIGHT = Display.getHeight() / BLOCK_SIZE;
+	public static final int MAP_WIDTH = Display.getWidth() / BLOCK_SIZE;
+	public static final int MAP_HEIGHT = Display.getHeight() / BLOCK_SIZE;
+	public static final int WORLD_WIDTH = 800;
+	public static final int WORLD_HEIGHT = 600;
 
 	public Player player;
-	
+
 	public Level() {
-		
+		world = new World();
+		Console.log("Generated World(" + WORLD_WIDTH + " * " + WORLD_HEIGHT + ")");
+		sc = new Scenery();
+		Console.log("Generated Sceneries");
 	}
-	
-	int pX = 0;
-	int pY;
+
+	private World world = null;
+	private Scenery sc = null;
 
 	public void render() {
-		World bg = new World();
-		Scenery sc = new Scenery();
-		bg.render(); 
+		world.render();
 		sc.render();
 	}
-	
+
 	public void tick() {
-		pX = player.getX();
-		Console.log("pX initialized! " + pX);
-		pY = player.y;
+		
 	}
-	
+
 	public void addPlayer(Player player) {
-		this.player = player;
+		this.player = player; 
 	}
 
 }
