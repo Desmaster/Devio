b2d6469f705bc255e4a18852c33cbf2eb1b93178
Timon de Groot <tdegroot96>
2012-11-05 15:23:19 +0100

    Refined

--- src/com/github/desmaster/Devio/realm/Scenery.java
+++ src/com/github/desmaster/Devio/realm/Scenery.java
@@ -1,26 +1,30 @@
 package com.github.desmaster.Devio.realm;
 
+import static com.github.desmaster.Devio.realm.Level.BLOCK_SIZE;
 import static com.github.desmaster.Devio.realm.Level.WORLD_HEIGHT;
 import static com.github.desmaster.Devio.realm.Level.WORLD_WIDTH;
-import static com.github.desmaster.Devio.realm.Level.BLOCK_SIZE;
-
-import com.github.desmaster.Devio.texture.iTexture;
+import static com.github.desmaster.Devio.texture.iTexture.ID_FLOWER_RED;
+import static com.github.desmaster.Devio.texture.iTexture.ID_FLOWER_YELLOW;
+import static com.github.desmaster.Devio.texture.iTexture.ID_GRASS;
+import static com.github.desmaster.Devio.texture.iTexture.objects;
 
 public class Scenery extends World {
 
 	public Scenery() {
 		for (int x = 0; x < WORLD_WIDTH; x++) {
 			for (int y = 0; y < WORLD_HEIGHT; y++) {
-				int seed = random.nextInt(iTexture.objects.length - random.nextInt(14));
-				//seed = random(0, iTexture.objects.length - random(0, 14));
-				if (random.nextInt(12 - random.nextInt(8)) == 1) {
-					if (seed == iTexture.ID_FLOWER_RED)
-						blocks[x][y] = new Block(iTexture.objects[seed], x * BLOCK_SIZE, y * BLOCK_SIZE);
-					if (seed == iTexture.ID_FLOWER_YELLOW)
-						blocks[x][y] = new Block(iTexture.objects[seed], x * BLOCK_SIZE, y * BLOCK_SIZE);
+				int seed;// = random.nextInt(objects.length -
+							// random.nextInt(14));
+				seed = random(0, objects.length);
+				if (random.nextInt(2) == 1) {
+						if (seed == ID_FLOWER_RED || seed == ID_FLOWER_YELLOW) {
+							if (blocks[x][y].texture == objects[ID_GRASS]) {
+							blocks[x][y] = new Block(objects[seed], x * BLOCK_SIZE, y * BLOCK_SIZE);
+							//Console.log("Rendered " + seed + " at x:" + x + ", y: " + y);
+							}
+					}
 				}
 			}
 		}
 	}
-
 }
