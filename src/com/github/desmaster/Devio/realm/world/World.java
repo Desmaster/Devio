7adc41dfd8cc7b120b2c970db6c793b34ccccfad
BrainCode <h4cker99@gmail.com>
2012-11-05 23:11:38 +0100

    Add saveWorld() and loadWorld()
    Dit is tijdelijk totdat we beter hebben

--- src/com/github/desmaster/Devio/realm/world/World.java
+++ src/com/github/desmaster/Devio/realm/world/World.java
@@ -2,6 +2,14 @@ package com.github.desmaster.Devio.realm.world;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
 import org.lwjgl.opengl.Display;
 
 import com.github.desmaster.Devio.cons.Console;
@@ -81,5 +89,34 @@ public class World {
 		glEnd();
 		glLoadIdentity();
 	}
+	
+	public void saveWorld(String filename, Tile[][] output_veld) {
+	     try {
+	        FileOutputStream fos = new FileOutputStream(filename);
+	        GZIPOutputStream gzos = new GZIPOutputStream(fos);
+	        ObjectOutputStream out = new ObjectOutputStream(gzos);
+	        out.writeObject(output_veld);
+	        out.flush();
+	        out.close();
+	     }
+	     catch (IOException e) {
+	         System.out.println(e); 
+	     }
+	  }
+
+	public Tile[][] loadWorld(String filename) {
+	      try {
+	        FileInputStream fis = new FileInputStream(filename);
+	        GZIPInputStream gzis = new GZIPInputStream(fis);
+	        ObjectInputStream in = new ObjectInputStream(gzis);
+	        Tile[][] gelezen_veld = (Tile[][])in.readObject();
+	        in.close();
+	        return gelezen_veld;
+	      }
+	      catch (Exception e) {
+	          System.out.println(e);
+	      }
+	      return null;
+	  }
 
 }
