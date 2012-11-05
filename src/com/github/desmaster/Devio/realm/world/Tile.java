ce9399c0239ca4dca4eeb16b6f2ce804d3b5f276
Timon de Groot <tdegroot96>
2012-11-05 19:42:38 +0100

    Fixed things

--- src/com/github/desmaster/Devio/realm/world/Tile.java
+++ src/com/github/desmaster/Devio/realm/world/Tile.java
@@ -8,64 +8,63 @@ import org.newdawn.slick.opengl.TextureLoader;
 import com.github.desmaster.Devio.cons.Console;
 
 public enum Tile {
-	GRASS("Grass",false,true), 						// 0
-	SAND("Sand",false,true),						// 1
-	STONE("Stone",false,true),						// 2
-	COBBLE_STONE("Cobblestone",false,true),			// 3
-	MOSSY_STONE("Mossy_cobblestone",false,true),	// 4
-	WATER("Water",false,true),						// 5
-	LAVA("Lava",false,true),						// 6
-	FLOWER_RED("Red_flower",false,true),			// 7
-	FLOWER_YELLOW("Yellow_flower",false,true),		// 8
-	FLOWER_BLUE("Blue_flower",false,true);			// 9
-	
+	GRASS("Grass", false, true), // 0
+	SAND("Sand", false, true), // 1
+	STONE("Stone", false, true), // 2
+	COBBLE_STONE("Cobblestone", false, true), // 3
+	MOSSY_STONE("Mossy_Cobblestone", false, true), // 4
+	WATER("Water", false, true); // 5
+	//LAVA("Lava", false, true) // 6
+
 	private String name;
 	private boolean breakable;
 	private boolean solid;
 	private Texture texture;
 	
-	Tile(String name,boolean breakable,boolean solid) {
+	public int x;
+	public int y;
+
+	Tile(String name, boolean breakable, boolean solid) {
 		this.name = name;
-        this.breakable = breakable;
-        this.solid = solid;
-        this.texture = loadTexture(name,".png");
-    }	
+		this.breakable = breakable;
+		this.solid = solid;
+		texture = loadTexture(name, "png");
+	}
+
 	private Texture loadTexture(String name, String ext) {
+		name = name.toLowerCase();
 		try {
-			Texture t1 = TextureLoader.getTexture(
-					ext.toUpperCase(),
-					getClass().getResourceAsStream("/img/" + name + "."
-							+ ext));
-			Console.log("Loaded " + name + "." + ext);
+			Console.log("Loading " + name + "." + ext);
+			Texture t1 = TextureLoader.getTexture(ext.toUpperCase(), getClass().getResourceAsStream("/img/" + name + "." + ext));
 			return t1;
 		} catch (IOException e) {
 			Console.log(e.getMessage());
 		}
 		return null;
 	}
-	
-	public int getID()
-	{
-		return this.ordinal();		
+
+	public int getID() {
+		return this.ordinal();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isBreakable() {
+		return breakable;
+	}
+
+	public boolean isSolid() {
+		return solid;
+	}
+
+	public Texture getTexture() {
+		return texture;
 	}
 	
-	public String getName()
-	{
-		return name;		
+	public void setTexture(Texture texture) {
+		this.texture = texture;
 	}
-	
-	public boolean isBreakable()
-	{
-		return breakable;		
-	}
-	
-	public boolean isSolid()
-	{
-		return solid;		
-	}	
-	
-	public Texture getTexture()
-	{
-		return texture;		
-	}
+
 }
