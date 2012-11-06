2c3602196c6c0d7469d669339ea5ce172c2d0a36
BrainCode <h4cker99@gmail.com>
2012-11-06 11:25:11 +0100

    Added a "face" to Mob

--- src/com/github/desmaster/Devio/realm/entity/Mob.java
+++ src/com/github/desmaster/Devio/realm/entity/Mob.java
@@ -3,6 +3,7 @@ package com.github.desmaster.Devio.realm.entity;
 public class Mob extends Entity {
 	
 	double lives;
+	int face = 0; // Up = 0 Right = 1 Down = 2 Left = 3
 	
 	public Mob(com.github.desmaster.Devio.util.Position spawnPosition, double lives) {
 		super(spawnPosition);
