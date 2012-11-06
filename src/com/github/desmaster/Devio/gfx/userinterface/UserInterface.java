37e8764d6ae19596469e3b6f0160f7dba550a72c
BrainCode <h4cker99@gmail.com>
2012-11-07 00:20:52 +0100

    Started userinterface class

---
+++ src/com/github/desmaster/Devio/gfx/userinterface/UserInterface.java
@@ -0,0 +1,22 @@
+package com.github.desmaster.Devio.gfx.userinterface;
+
+import java.awt.Color;
+
+import org.lwjgl.util.Rectangle;
+
+public class UserInterface {
+
+	//State
+	private boolean active = false;
+	
+	//Info
+	private String name = "Undefined";
+	
+	//Look & Feel
+	private boolean Enabled_Titlebar = true;
+	private boolean Enabled_Title = true;
+	private boolean Enabled_CloseButton = true;
+	private Rectangle container = new Rectangle(0,0,0,0);
+	private Color color = Color.white;
+	
+}
