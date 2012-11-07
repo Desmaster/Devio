8506194b8c3be6aeb19cb5dbc6c5e227c63192e1
Timon de Groot <tdegroot96>
2012-11-07 22:29:36 +0100

    Formatted

--- src/com/github/desmaster/Devio/realm/world/World.java
+++ src/com/github/desmaster/Devio/realm/world/World.java
@@ -20,11 +20,11 @@ public class World {
 		worldtiles[1][1] = Tile.STONE;
 		worldtiles[1][2] = Tile.STONE;
 		worldtiles[1][3] = Tile.STONE;
-		
+
 		worldtiles[2][1] = Tile.STONE;
 		worldtiles[2][2] = Tile.MOSSY_STONE;
 		worldtiles[2][3] = Tile.STONE;
-		
+
 		worldtiles[3][1] = Tile.STONE;
 		worldtiles[3][2] = Tile.STONE;
 		worldtiles[3][3] = Tile.STONE;
@@ -46,40 +46,44 @@ public class World {
 				SubArea[xx][yy] = worldtiles[x + xx][y + yy];
 			}
 		}
-		
+
 		return SubArea;
 	}
 
 	public Tile[][] getVisibleMap(Player player) {
-		
+
 		Position offset = getVisibleMapOffsetPosition(player);
-		
-		return getSubArea(offset.getX(),offset.getY(),Realm.MAP_WIDTH,Realm.MAP_HEIGHT);
+
+		return getSubArea(offset.getX(), offset.getY(), Realm.MAP_WIDTH, Realm.MAP_HEIGHT);
 	}
-	
+
 	public Position getVisibleMapOffsetPosition(Player player) {
-		
-		int offsetXTile = player.x  - (int) Math.ceil(Realm.MAP_WIDTH/2);
-		int offsetYTile = player.y  - (int) Math.ceil(Realm.MAP_HEIGHT/2);
-		
-		if (offsetXTile < 0) offsetXTile = 0;
-		if (offsetYTile < 0) offsetYTile = 0;
-		
-		if (offsetXTile > Realm.WORLD_WIDTH - Realm.MAP_WIDTH) offsetXTile = Realm.WORLD_WIDTH - Realm.MAP_WIDTH;
-		if (offsetYTile > Realm.WORLD_HEIGHT - Realm.MAP_HEIGHT) offsetYTile = Realm.WORLD_HEIGHT - Realm.MAP_HEIGHT;
-		
-		return new Position(offsetXTile,offsetYTile);
+
+		int offsetXTile = player.x - (int) Math.ceil(Realm.MAP_WIDTH / 2);
+		int offsetYTile = player.y - (int) Math.ceil(Realm.MAP_HEIGHT / 2);
+
+		if (offsetXTile < 0)
+			offsetXTile = 0;
+		if (offsetYTile < 0)
+			offsetYTile = 0;
+
+		if (offsetXTile > Realm.WORLD_WIDTH - Realm.MAP_WIDTH)
+			offsetXTile = Realm.WORLD_WIDTH - Realm.MAP_WIDTH;
+		if (offsetYTile > Realm.WORLD_HEIGHT - Realm.MAP_HEIGHT)
+			offsetYTile = Realm.WORLD_HEIGHT - Realm.MAP_HEIGHT;
+
+		return new Position(offsetXTile, offsetYTile);
 	}
 
 	public void render() {
 		Tile[][] SubArea = getVisibleMap(Screen.getPlayer());
-		for(int x = 0; x < SubArea.length; x++) {
+		for (int x = 0; x < SubArea.length; x++) {
 			for (int y = 0; y < SubArea[0].length; y++) {
 				renderTile(x, y, SubArea[x][y]);
 			}
 		}
 	}
-	
+
 	public void renderTile(int x, int y, Tile tile) {
 		tile.getTexture().bind();
 		x *= Realm.BLOCK_SIZE / 2;
@@ -89,24 +93,24 @@ public class World {
 		glLoadIdentity();
 		glTranslatef(x, y, 0);
 		glBegin(GL_QUADS);
-			glTexCoord2f(0, 0);
-			glVertex2f(x, y);
-			glTexCoord2f(1, 0);
-			glVertex2f(x + width, y);
-			glTexCoord2f(1, 1);
-			glVertex2f(x + width, y + height);
-			glTexCoord2f(0, 1);
-			glVertex2f(x, y + height);
+		glTexCoord2f(0, 0);
+		glVertex2f(x, y);
+		glTexCoord2f(1, 0);
+		glVertex2f(x + width, y);
+		glTexCoord2f(1, 1);
+		glVertex2f(x + width, y + height);
+		glTexCoord2f(0, 1);
+		glVertex2f(x, y + height);
 		glEnd();
 		glLoadIdentity();
 	}
-	
+
 	public Tile[][] saveWorld() {
-	     return worldtiles;
+		return worldtiles;
 	}
 
 	public void loadWorld(Tile[][] tiles) {
-	      worldtiles = tiles;
-	  }
+		worldtiles = tiles;
+	}
 
 }
