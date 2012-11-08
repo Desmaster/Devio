d002c68d0090da6188eee4dea40bf0c3dbbbf04c
BrainCode <h4cker99@gmail.com>
2012-11-08 01:02:28 +0100

    GameObject support v2 checkCollision

--- src/com/github/desmaster/Devio/realm/gameobject/GameObject.java
+++ src/com/github/desmaster/Devio/realm/gameobject/GameObject.java
@@ -6,23 +6,23 @@ import com.github.desmaster.Devio.tex.iTexture;
 
 public enum GameObject {
 	
-	RED_FLOWER("Red_Flower", true, false, iTexture.ID_FLOWER_RED),
-	YELLOW_FLOWER("Yellow_Flower", true, false, iTexture.ID_FLOWER_YELLOW),
-	BRICK_WALL("Brick_wall",false,true,iTexture.ID_BRICK_WALL);
+	RED_FLOWER("Red_Flower", true, false, false, false, false, iTexture.ID_FLOWER_RED),
+	YELLOW_FLOWER("Yellow_Flower", true, false, false, false, false, iTexture.ID_FLOWER_YELLOW),
+	BRICK_WALL("Brick_wall",false, true, true, true, true,iTexture.ID_BRICK_WALL);
 	
 	private String name;
 	private boolean breakable;
-	private boolean solid;
+	private boolean[] solid = new boolean[4];
 	private int TextureID;
 	
 	public int x;
 	public int y;
 
-	GameObject(String name, boolean breakable, boolean solid,int TextureID) {
+	GameObject(String name, boolean breakable, boolean north_solid, boolean east_solid, boolean south_solid, boolean west_solid,int TextureID) {
 		Console.log("Initialized GameObject: " + name);
 		this.name = name;
 		this.breakable = breakable;
-		this.solid = solid;
+		this.solid = new boolean[]{north_solid, east_solid, south_solid, west_solid};
 		this.TextureID = TextureID;
 	}
 
@@ -38,8 +38,8 @@ public enum GameObject {
 		return breakable;
 	}
 
-	public boolean isSolid() {
-		return solid;
+	public boolean checkCollision(int face) {
+		return solid[face];
 	}
 
 	public Texture getTexture() {
