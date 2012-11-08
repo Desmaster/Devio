24dc752cd7685ce9278b22130cd49d0d8c27a34c
Timon de Groot <tdegroot96>
2012-11-08 09:35:58 +0100

    Improved some things

--- src/com/github/desmaster/Devio/gfx/userinterface/userinterfaces/Interface_Console.java
+++ src/com/github/desmaster/Devio/gfx/userinterface/userinterfaces/Interface_Console.java
@@ -1,28 +1,24 @@
 package com.github.desmaster.Devio.gfx.userinterface.userinterfaces;
 
-import java.awt.Color;
-
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.Color;
 import org.lwjgl.util.Rectangle;
 
 import com.github.desmaster.Devio.gfx.Screen;
 import com.github.desmaster.Devio.gfx.userinterface.UserInterface;
-import com.github.desmaster.Devio.realm.Realm;
 
-public class Interface_Console extends UserInterface{
+public class Interface_Console extends UserInterface {
 
 	public Interface_Console() {
 		super("Console");
-		container = new Rectangle(20,20,Realm.MAP_WIDTH - 40,Realm.MAP_HEIGHT / 4 - 40);
-		color = new Color(0,0,0,166);
+		container = new Rectangle(0, 0, Display.getWidth(), (Display.getWidth() / 2) - 150);
+		color = new Color(0, 0, 0, 166);
 	}
-	
-	public void tick(){
-		if (Screen.getInput().grave.down) {
-			Screen.getInput().releaseAll();
+
+	public void tick() {
+		if (Screen.getInput().grave.clicked) {
 			active = !active;
 		}
 	}
-	
-	
 
 }
