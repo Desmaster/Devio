9e433aa08ee31acf4640addf346129c88385b7b2
BrainCode <h4cker99@gmail.com>
2012-11-09 11:00:31 +0100

    Moved console render

--- src/com/github/desmaster/Devio/gfx/userinterface/UserInterface.java
+++ src/com/github/desmaster/Devio/gfx/userinterface/UserInterface.java
@@ -23,23 +23,7 @@ public class UserInterface {
 	}
 
 	public void render() {
-		if (active) {
-			glLoadIdentity();
-			glDisable(GL_TEXTURE_2D);
-			glDisable(GL_BLEND);
-			glColor4f(0.15f, 0.15f, 0.15f, 0.5f);
-			glTranslatef(container.getX(), container.getY(), 0);
-			glBegin(GL_QUADS);
-				glVertex2i(container.getX(), container.getY());
-				glVertex2i(container.getX() + container.getWidth(), container.getY());
-				glVertex2i(container.getX() + container.getWidth(), container.getY() + container.getHeight());
-				glVertex2i(container.getX(), container.getY() + container.getHeight());
-			glEnd();
-			glColor4f(1, 1, 1, 1);
-			glEnable(GL_BLEND);
-			glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-			
-		}
+
 	}
 
 	public void open() {
--- src/com/github/desmaster/Devio/gfx/userinterface/userinterfaces/Interface_Console.java
+++ src/com/github/desmaster/Devio/gfx/userinterface/userinterfaces/Interface_Console.java
@@ -1,5 +1,20 @@
 package com.github.desmaster.Devio.gfx.userinterface.userinterfaces;
 
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glColor4f;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glTranslatef;
+import static org.lwjgl.opengl.GL11.glVertex2i;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.Color;
 import org.lwjgl.util.Rectangle;
@@ -20,5 +35,37 @@ public class Interface_Console extends UserInterface {
 			active = !active;
 		}
 	}
-
+	
+	public void render() {
+		if (active) {
+			renderContainer();
+			renderConsole();
+		}
+	}
+	
+	public void renderContainer(){
+		glLoadIdentity();
+		glDisable(GL_TEXTURE_2D);
+		glColor4f(0.15f, 0.15f, 0.15f, 0.4f);
+		glTranslatef(container.getX(), container.getY(), 0);
+		glBegin(GL_QUADS);
+			glVertex2i(container.getX(), container.getY());
+			glVertex2i(container.getX() + container.getWidth(), container.getY());
+			glVertex2i(container.getX() + container.getWidth(), container.getY() + container.getHeight());
+			glVertex2i(container.getX(), container.getY() + container.getHeight());
+		glEnd();
+	}
+	
+	public void renderConsole(){
+		glLoadIdentity();
+		glDisable(GL_TEXTURE_2D);
+		glColor4f(0.05f, 0.05f, 0.05f, 0.8f);
+		glTranslatef(container.getX(), container.getY(), 0);
+		glBegin(GL_QUADS);
+			glVertex2i(container.getX() + 10, container.getY() + 10);
+			glVertex2i(container.getX() + container.getWidth() - 10, container.getY() + 10);
+			glVertex2i(container.getX() + container.getWidth() - 10, container.getY() + container.getHeight() - 10);
+			glVertex2i(container.getX() + 10, container.getY() + container.getHeight() - 10);
+		glEnd();
+	}
 }
