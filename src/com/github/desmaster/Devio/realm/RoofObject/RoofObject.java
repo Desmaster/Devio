b81710c4591b047768ee0b08177a187d561ffcc1
Timon de Groot <tdegroot96>
2012-11-09 09:42:11 +0100

    Like this new boolean?

---
+++ src/com/github/desmaster/Devio/realm/RoofObject/RoofObject.java
@@ -0,0 +1,64 @@
+package com.github.desmaster.Devio.realm.RoofObject;
+
+import org.newdawn.slick.opengl.Texture;
+
+import com.github.desmaster.Devio.cons.Console;
+import com.github.desmaster.Devio.tex.iTexture;
+
+public enum RoofObject {
+	
+	GRASS("Grass", false, new boolean[] {false, false, false, false}, iTexture.ID_GRASS),
+	SAND("Sand", false, new boolean[] {false, false, false, false}, iTexture.ID_SAND),
+	STONE("Stone", false, new boolean[] {false, false, false, false}, iTexture.ID_STONE),
+	COBBLE_STONE("Cobblestone", false, new boolean[] {false, false, false, false}, iTexture.ID_COBBLESTONE),
+	MOSSY_STONE("Mossy_Cobblestone", false, new boolean[] {false, false, false, false}, iTexture.ID_MOSS_STONE);
+
+	private String name;
+	private boolean breakable;
+	private boolean[] solid = new boolean[4];
+	private int TextureID;
+	
+	public int x;
+	public int y;
+
+	RoofObject(String name, boolean breakable, boolean[] test, int TextureID) {
+		Console.log("Initialized RoofObject: " + name);
+		this.name = name;
+		this.breakable = breakable;
+		this.solid = test;
+		this.TextureID = TextureID;
+	}
+
+	public int getID() {
+		return this.ordinal();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getDisplayName() {
+		return name.replaceAll("_", " ");
+	}
+
+	public boolean isBreakable() {
+		return breakable;
+	}
+
+	public boolean checkCollision(int face) {
+		return solid[face];
+	}
+
+	public Texture getTexture() {
+		return iTexture.textures[TextureID];
+	}
+	
+	public int getTextureID() {
+		return TextureID;
+	}
+	
+	public void setTextureID(int TextureID) {
+		this.TextureID = TextureID;
+	}
+
+}
