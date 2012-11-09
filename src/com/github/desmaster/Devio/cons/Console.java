48b6123942dca3f29c157887df98f20a8d72c0a7
Timon de Groot <tdegroot96>
2012-11-09 19:24:14 +0100

    Formatted + Input Blocking while in console

--- src/com/github/desmaster/Devio/cons/Console.java
+++ src/com/github/desmaster/Devio/cons/Console.java
@@ -29,39 +29,45 @@ public class Console extends UserInterface {
 	private static int textX;
 	private int textRenderX = 15;
 	private int textRenderY = 144;
-	
+
 	boolean shouldRenderLine = true;
 	int line;
 	int lineDelay = 35;
 	int lineX = 12;
-	
+
 	public Console() {
 		super("Console");
 		container = new Rectangle(0, 0, Display.getWidth(), (Display.getHeight() / 4));
 	}
-	
+
 	public void tick() {
 		if (Screen.getInput().grave.clicked) {
 			active = !active;
 		}
-		
+
+		if (active) {
+			Screen.getPlayer().disableInput();
+		} else {
+			Screen.getPlayer().enableInput();
+		}
+
 		if (shouldRenderLine) {
 			if (line < lineDelay)
-				line ++;
+				line++;
 		} else {
 			line--;
 		}
-		
+
 		if (line == lineDelay)
 			shouldRenderLine = false;
-		
+
 		if (line == 00)
 			shouldRenderLine = true;
-		
+
 		lineX = textX + 3;
 		pollInput();
 	}
-	
+
 	public void pollInput() {
 		while (Keyboard.next()) {
 			if (Keyboard.getEventKeyState()) {
@@ -69,7 +75,7 @@ public class Console extends UserInterface {
 			}
 		}
 	}
-	
+
 	public void render() {
 		if (active) {
 			renderContainer();
@@ -79,40 +85,40 @@ public class Console extends UserInterface {
 			msg = "";
 		}
 	}
-	
+
 	public void renderContainer() {
 		glLoadIdentity();
 		glDisable(GL_TEXTURE_2D);
 		glColor4f(0.15f, 0.15f, 0.15f, 0.8f);
 		glTranslatef(container.getX(), container.getY(), 0);
 		glBegin(GL_QUADS);
-			glVertex2f(container.getX(), container.getY());
-			glVertex2f(container.getX() + container.getWidth(), container.getY());
-			glVertex2f(container.getX() + container.getWidth(), container.getY() + container.getHeight());
-			glVertex2f(container.getX(), container.getY() + container.getHeight());
+		glVertex2f(container.getX(), container.getY());
+		glVertex2f(container.getX() + container.getWidth(), container.getY());
+		glVertex2f(container.getX() + container.getWidth(), container.getY() + container.getHeight());
+		glVertex2f(container.getX(), container.getY() + container.getHeight());
 		glEnd();
 		glColor4f(1, 1, 1, 1);
 		if (shouldRenderLine) {
-		glBegin(GL_LINE_STRIP);
+			glBegin(GL_LINE_STRIP);
 			glVertex2f(lineX, 132);
 			glVertex2f(lineX, 147);
-		glEnd();
+			glEnd();
 		}
 	}
-	
+
 	public void renderConsole() {
 		glLoadIdentity();
 		glDisable(GL_TEXTURE_2D);
 		glColor4f(0.05f, 0.05f, 0.05f, 0.8f);
 		glTranslatef(container.getX(), container.getY(), 0);
 		glBegin(GL_QUADS);
-			glVertex2i(container.getX() + 10, container.getY() + 10);
-			glVertex2i(container.getX() + container.getWidth() - 10, container.getY() + 10);
-			glVertex2i(container.getX() + container.getWidth() - 10, container.getY() + container.getHeight() - 40);
-			glVertex2i(container.getX() + 10, container.getY() + container.getHeight() - 40);
+		glVertex2i(container.getX() + 10, container.getY() + 10);
+		glVertex2i(container.getX() + container.getWidth() - 10, container.getY() + 10);
+		glVertex2i(container.getX() + container.getWidth() - 10, container.getY() + container.getHeight() - 40);
+		glVertex2i(container.getX() + 10, container.getY() + container.getHeight() - 40);
 		glEnd();
 	}
-	
+
 	public void renderInputText() {
 		glLoadIdentity();
 		glDisable(GL_TEXTURE_2D);
@@ -138,13 +144,13 @@ public class Console extends UserInterface {
 		log(msg);
 		Color.white.bind();
 		drawString(msg, 50, 50);
-		
+
 	}
 
 	public static void setMessage(String s) {
 		msg = s;
 	}
-	
+
 	public static void drawString(String s, int x, int y) {
 		int startX = x;
 		GL11.glDisable(GL11.GL_BLEND);
