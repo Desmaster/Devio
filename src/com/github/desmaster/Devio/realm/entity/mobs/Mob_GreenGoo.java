9c9ddbae5a32e9bbac05185aed72a1ab824b160c
BrainCode <h4cker99@gmail.com>
2012-11-10 13:19:25 +0100

    Added Mob_GreenGoo

---
+++ src/com/github/desmaster/Devio/realm/entity/mobs/Mob_GreenGoo.java
@@ -0,0 +1,13 @@
+package com.github.desmaster.Devio.realm.entity.mobs;
+
+import com.github.desmaster.Devio.realm.entity.Mob;
+import com.github.desmaster.Devio.util.Position;
+
+public class Mob_GreenGoo extends Mob{
+	
+	public Mob_GreenGoo(Position spawnPosition, double lives) {
+		super(new Position(10,15), 50);
+		
+	}
+
+}
