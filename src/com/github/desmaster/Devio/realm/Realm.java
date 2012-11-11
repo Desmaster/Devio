6744c366d3dc1f981f0465da08a93240920d4c96
Timon de Groot <tdegroot96>
2012-11-11 04:27:48 +0100

    +

--- src/com/github/desmaster/Devio/realm/Realm.java
+++ src/com/github/desmaster/Devio/realm/Realm.java
@@ -4,7 +4,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 
 import com.github.desmaster.Devio.cons.Console;
-import com.github.desmaster.Devio.realm.entity.MobHandeler;
+import com.github.desmaster.Devio.realm.entity.MobHandler;
 import com.github.desmaster.Devio.realm.entity.Player;
 import com.github.desmaster.Devio.realm.gameobject.Scenery;
 import com.github.desmaster.Devio.realm.roofobject.Roof;
@@ -13,15 +13,15 @@ import com.github.desmaster.Devio.realm.world.World;
 public class Realm {
 
 	public static final int BLOCK_SIZE = 32;
-	public static final int MAP_WIDTH = Display.getWidth() / BLOCK_SIZE;
-	public static final int MAP_HEIGHT = Display.getHeight() / BLOCK_SIZE;
+	public static final int MAP_WIDTH = Display.getWidth() / BLOCK_SIZE; // 25
+	public static final int MAP_HEIGHT = Display.getHeight() / BLOCK_SIZE; // 20
 	public static final int WORLD_WIDTH = 1024;
 	public static final int WORLD_HEIGHT = 1024;
 
 	public static Player player;
 	public static World world = null;
 	public static Scenery sc = null;
-	public static MobHandeler mobhandeler = null;
+	public static MobHandler mobhandler = null;
 	public static Roof roof = null;
 
 	public Realm() {
@@ -29,7 +29,7 @@ public class Realm {
 		Console.log("Generated World(" + WORLD_WIDTH + " * " + WORLD_HEIGHT+ ")");
 		sc = new Scenery();
 		Console.log("Generated Sceneries");
-		mobhandeler = new MobHandeler();
+		mobhandler = new MobHandler();
 		Console.log("Initialized Mob Handeler");
 		roof = new Roof();
 		Console.log("Initialized Roof");
@@ -38,12 +38,12 @@ public class Realm {
 	public void render() {
 		world.render();
 		sc.render();
-		mobhandeler.render();
+		mobhandler.render();
 		roof.render();
 	}
 
 	public void tick() {
-		mobhandeler.tick();
+		mobhandler.tick();
 	}
 
 	public void addPlayer(Player player) {
