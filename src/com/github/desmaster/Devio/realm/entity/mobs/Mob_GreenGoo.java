c5fefa810c5d0ee7fd1ca6e9e2b16e41191b830b
Timon de Groot <tdegroot96>
2012-11-11 22:41:32 +0100

    Moved tick to EvilMob

--- src/com/github/desmaster/Devio/realm/entity/mobs/Mob_GreenGoo.java
+++ src/com/github/desmaster/Devio/realm/entity/mobs/Mob_GreenGoo.java
@@ -1,54 +1,16 @@
 package com.github.desmaster.Devio.realm.entity.mobs;
 
-import com.github.desmaster.Devio.cons.Console;
-import com.github.desmaster.Devio.realm.Realm;
-import com.github.desmaster.Devio.realm.entity.Mob;
+import com.github.desmaster.Devio.realm.entity.EvilMob;
 import com.github.desmaster.Devio.tex.iTexture;
 import com.github.desmaster.Devio.util.Position;
-import com.github.desmaster.Devio.util.gamemath.Collision;
-import com.github.desmaster.Devio.util.gamemath.Distance;
 
-public class Mob_GreenGoo extends Mob {
+public class Mob_GreenGoo extends EvilMob {
 
 	public Mob_GreenGoo() {
 		super(new Position(10, 15), 50);
 		setTexture(iTexture.ID_GOO_GREEN);
 	}
 
-	public void tick() {
-		walkblock();
-		if (!getWalkBlock()) {
-			int[] direction = Distance.getDirection(x, y, Realm.player.x, Realm.player.y);
-
-			// Console.log(x+" "+y+" "+Realm.player.x+" "+Realm.player.y);
-			// Console.log(direction[0]+" "+direction[1]);
-
-			if (!(Collision.check(new Position(x, y), direction[0]))) {
-				switch (direction[0]) {
-				case 0:
-					walkUp();
-				case 1:
-					walkRight();
-				case 2:
-					walkDown();
-				case 3:
-					walkLeft();
-				}
-			} else if (!(Collision.check(new Position(x, y), direction[1]))) {
-				switch (direction[1]) {
-				case 0:
-					walkUp();
-				case 1:
-					walkRight();
-				case 2:
-					walkDown();
-				case 3:
-					walkLeft();
-				}
-			} else {
-				setWalkBlock(120);
-			}
-		}
-	}
+	
 
 }
