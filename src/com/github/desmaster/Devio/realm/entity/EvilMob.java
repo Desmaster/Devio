1b7bcba05b5ba222b3eb66b326b8ad8cabd05082
BrainCode <h4cker99@gmail.com>
2012-11-12 00:47:14 +0100

    Attack Players

--- src/com/github/desmaster/Devio/realm/entity/EvilMob.java
+++ src/com/github/desmaster/Devio/realm/entity/EvilMob.java
@@ -6,7 +6,11 @@ import com.github.desmaster.Devio.util.gamemath.Collision;
 import com.github.desmaster.Devio.util.gamemath.Distance;
 
 public class EvilMob extends Mob {
-
+	
+	public int damage_Hit = 5;
+	public int damage_Critical = 10;
+	public int damage_timeout = 40;
+	
 	public EvilMob(Position spawnPosition, double lives) {
 		super(spawnPosition, lives);
 	}
@@ -22,24 +26,24 @@ public class EvilMob extends Mob {
 			if (!(Collision.check(new Position(x, y), direction[0]))) {
 				switch (direction[0]) {
 				case 0:
-					walkUp();
+					aiUp();
 				case 1:
-					walkRight();
+					aiRight();
 				case 2:
-					walkDown();
+					aiDown();
 				case 3:
-					walkLeft();
+					aiLeft();
 				}
 			} else if (!(Collision.check(new Position(x, y), direction[1]))) {
 				switch (direction[1]) {
 				case 0:
-					walkUp();
+					aiUp();
 				case 1:
-					walkRight();
+					aiRight();
 				case 2:
-					walkDown();
+					aiDown();
 				case 3:
-					walkLeft();
+					aiLeft();
 				}
 			} else {
 				setWalkBlock(120);
@@ -47,4 +51,36 @@ public class EvilMob extends Mob {
 		}
 	}
 	
+	public void aiUp(){
+		if (Realm.player.x == x && Realm.player.y == (y-1)){
+			hurt(damage_Hit,Realm.player);
+		} else {
+			walkUp();
+		}
+	}
+	
+	public void aiRight(){
+		if (Realm.player.x == (x+1) && Realm.player.y == y){
+			hurt(damage_Hit,Realm.player);
+		} else {
+			walkRight();
+		}
+	}
+		
+	public void aiDown(){
+		if (Realm.player.x == x && Realm.player.y == (y+1)){
+			hurt(damage_Hit,Realm.player);
+		} else {
+			walkDown();
+		}
+	}
+
+	public void aiLeft(){
+		if (Realm.player.x == (x - 1) && Realm.player.y == y){
+			hurt(damage_Hit,Realm.player);
+		} else {
+			walkLeft();
+		}
+	}
+
 }
