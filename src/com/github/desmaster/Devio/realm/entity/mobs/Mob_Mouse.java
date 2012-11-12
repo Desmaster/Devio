653fb2f2673421ebeb6f14c43c8986505a6bfcca
BrainCode <h4cker99@gmail.com>
2012-11-12 22:24:11 +0100

    slower

--- src/com/github/desmaster/Devio/realm/entity/mobs/Mob_Mouse.java
+++ src/com/github/desmaster/Devio/realm/entity/mobs/Mob_Mouse.java
@@ -1,6 +1,5 @@
 package com.github.desmaster.Devio.realm.entity.mobs;
 
-import com.github.desmaster.Devio.realm.entity.FriendlyMob;
 import com.github.desmaster.Devio.realm.entity.FriendlyMob2;
 import com.github.desmaster.Devio.tex.iTexture;
 import com.github.desmaster.Devio.util.Position;
@@ -10,7 +9,7 @@ public class Mob_Mouse extends FriendlyMob2 {
 	public Mob_Mouse() {
 		super(new Position(5, 5), 45);
 		setTexture(iTexture.ID_MOUSE_0);
-		setWalkSpeed(4);
+		setWalkSpeed(12);
 	}
 
 }
