ab7faad6af04019457470ffa076f2749503494ed
BrainCode <h4cker99@gmail.com>
2012-11-12 21:48:50 +0100

    Use new AI

--- src/com/github/desmaster/Devio/realm/entity/mobs/Mob_Mouse.java
+++ src/com/github/desmaster/Devio/realm/entity/mobs/Mob_Mouse.java
@@ -1,14 +1,16 @@
 package com.github.desmaster.Devio.realm.entity.mobs;
 
 import com.github.desmaster.Devio.realm.entity.FriendlyMob;
+import com.github.desmaster.Devio.realm.entity.FriendlyMob2;
 import com.github.desmaster.Devio.tex.iTexture;
 import com.github.desmaster.Devio.util.Position;
 
-public class Mob_Mouse extends FriendlyMob{
+public class Mob_Mouse extends FriendlyMob2 {
 
 	public Mob_Mouse() {
 		super(new Position(5, 5), 45);
 		setTexture(iTexture.ID_MOUSE_0);
+		setWalkSpeed(4);
 	}
 
 }
