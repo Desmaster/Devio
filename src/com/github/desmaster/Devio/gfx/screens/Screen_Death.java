f14cc3bf4ebff446447b33bad012d87efd1130d0
BrainCode <h4cker99@gmail.com>
2012-11-12 02:03:42 +0100

    YOU ARE DEATH

---
+++ src/com/github/desmaster/Devio/gfx/screens/Screen_Death.java
@@ -0,0 +1,47 @@
+package com.github.desmaster.Devio.gfx.screens;
+
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.opengl.Display;
+
+import com.github.desmaster.Devio.cons.Console;
+import com.github.desmaster.Devio.gfx.Screen;
+import com.github.desmaster.Devio.gfx.Screen.State;
+import com.github.desmaster.Devio.realm.Realm;
+import com.github.desmaster.Devio.tex.iTexture;
+
+public class Screen_Death {
+	
+	public static void tick(){
+		if (Screen.getInput().enter.clicked) {
+			Realm.player.setLives(100);
+			Realm.player.setX(2);
+			Realm.player.setY(2);
+			Screen.state = State.GAME;
+		}
+	}
+	
+	public static void render(){
+		tick();		
+		//glEnable(GL_TEXTURE_2D);
+		iTexture.textures[iTexture.ID_SCREEN_DEATH].bind();
+		glLoadIdentity();
+		glTranslatef(0, 0, 0);
+		glBegin(GL_QUADS);
+		
+		glTexCoord2f(0, 0);
+		glVertex2f(0, 0);
+		
+		glTexCoord2f(1, 0);
+		glVertex2f(Display.getWidth(), 0);
+		
+		glTexCoord2f(1, 1);
+		glVertex2f(Display.getWidth(), Display.getHeight());
+		
+		glTexCoord2f(0, 1);
+		glVertex2f(0, Display.getHeight());
+		
+		glEnd();
+		glLoadIdentity();
+	}
+}
