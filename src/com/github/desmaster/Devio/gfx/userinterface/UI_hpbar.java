3345c8ff5cae88652ae981ab34aed17503d3c283
BrainCode <h4cker99@gmail.com>
2012-11-12 19:57:14 +0100

    YAY :P

--- src/com/github/desmaster/Devio/gfx/userinterface/UI_hpbar.java
+++ src/com/github/desmaster/Devio/gfx/userinterface/UI_hpbar.java
@@ -5,18 +5,18 @@ import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
 import static org.lwjgl.opengl.GL11.glTexCoord2f;
-import static org.lwjgl.opengl.GL11.glTranslatef;
 import static org.lwjgl.opengl.GL11.glVertex2f;
 
 import org.lwjgl.opengl.Display;
 
 import com.github.desmaster.Devio.cons.Console;
+import com.github.desmaster.Devio.realm.Realm;
 import com.github.desmaster.Devio.tex.iTexture;
 
 public class UI_hpbar extends UserInterface {
 
 	public int x1 = (Display.getWidth() - 512)/2;
-	public int y1 = Display.getHeight() - 32;
+	public int y1 = Display.getHeight() - 16;
 	public int x2 = (Display.getWidth() - 512)/2+512;
 	public int y2 = Display.getHeight();
 	
@@ -30,15 +30,14 @@ public class UI_hpbar extends UserInterface {
 	
 	public void render() {
 		renderEmpty();
+		renderFull((int)Realm.player.getLives());
 	}
 	
 	
 	
 	public void renderEmpty() {
-		Console.log(x1+" "+y1+" "+x2+" "+y2);
 		iTexture.textures[iTexture.ID_HP_EMPTY].bind();
 		glLoadIdentity();
-		glTranslatef(x1, y1, 0);
 		glBegin(GL_QUADS);
 		glTexCoord2f(0, 0);
 		glVertex2f(x1, y1);
@@ -50,6 +49,26 @@ public class UI_hpbar extends UserInterface {
 		glVertex2f(x1, y2);
 		glEnd();
 		glLoadIdentity();
-		Console.log("RENDER!");
+	}
+	
+	public void renderFull(int amount) {
+		double i = 512;
+		i /= 100;
+		i *= amount;
+		int amound = 512 - (int)i;
+		Console.log(x2 + " " + amound + " " + (int) amount);
+		iTexture.textures[iTexture.ID_HP_FULL].bind();
+		glLoadIdentity();
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(x1, y1);
+		glTexCoord2f(1, 0);
+		glVertex2f(x2 - amound, y1);
+		glTexCoord2f(1, 1);
+		glVertex2f(x2 - amound, y2);
+		glTexCoord2f(0, 1);
+		glVertex2f(x1, y2);
+		glEnd();
+		glLoadIdentity();
 	}
 }
